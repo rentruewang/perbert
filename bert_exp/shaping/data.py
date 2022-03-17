d0d661eed3ad01aeb0dc4dcaef23e893cc9167ec
RenChu Wang <patrick1031wang@gmail.com>
2022-03-18 01:30:56 +0800

    😘 Finished wrapping Dataset (datasets) into Dataset (torch).
    Yes, indeed. It causes a lot of confusion. And huggingface's code simply ignores the differences.

---
+++ bert_exp/shaping/data.py
@@ -0,0 +1,43 @@
+# pyright: reportPrivateImportUsage=false
+from __future__ import annotations
+from . import datasets
+
+import typing
+from pathlib import Path
+
+import datasets
+from datasets import DatasetDict
+from pytorch_lightning import LightningDataModule
+from torch.utils.data import Dataset
+
+
+class WikiTextDataModule(LightningDataModule):
+    def __init__(
+        self, batch_size: int, data_dir: str | Path = "wikitext/wikitext-103-v1"
+    ) -> None:
+        super().__init__()
+
+        self.batch_size = batch_size
+        self.data_dir = Path(data_dir)
+
+        self.prepare_data()
+
+    def prepare_data(self):
+        parent = str(self.data_dir.parent)
+        child = str(self.data_dir.name)
+        self.dataset = typing.cast(DatasetDict, datasets.load_dataset(parent, child))
+
+    @property
+    def train_dataset(self) -> Dataset:
+        return datasets.wrap(self.dataset["train"])
+
+    @property
+    def test_dataset(self) -> Dataset:
+        return datasets.wrap(self.dataset["test"])
+
+    @property
+    def validation_dataset(self) -> Dataset:
+        return datasets.wrap(self.dataset["validation"])
+
+    def setup(self, stage: str | None = None) -> None:
+        return super().setup(stage)
---
+++ bert_exp/shaping/datasets.py
@@ -0,0 +1,37 @@
+from abc import abstractmethod
+from socketserver import DatagramRequestHandler
+from typing import Any, Generic, Protocol, Sequence, TypeVar
+from torch.utils.data import Dataset
+
+# TODO: documentation
+
+T = TypeVar("T")
+K = TypeVar("K", contravariant=True)
+V = TypeVar("V", covariant=True)
+
+
+class Indexable(Protocol[K, V]):
+    @abstractmethod
+    def __len__(self) -> int:
+        ...
+
+    @abstractmethod
+    def __getitem__(self, key: K) -> V:
+        ...
+
+
+class _GeneratedDataset(Dataset, Generic[T]):
+    def __init__(self, seq: Indexable[int, T]) -> None:
+        super().__init__()
+
+        self._seq = seq
+
+    def __len__(self) -> int:
+        return len(self._seq)
+
+    def __getitem__(self, key: int) -> T:
+        return self._seq[key]
+
+
+def wrap(seq: Indexable[int, T]) -> Dataset[T]:
+    return _GeneratedDataset[T](seq)
