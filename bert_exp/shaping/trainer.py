303f3ea1694ff8f791a5d4550d703b16487e956f
RenChu Wang <patrick1031wang@gmail.com>
2022-03-22 20:34:55 +0800

    🏁 Fun with flags.
    Added several more flags.

--- bert_exp/shaping/trainer.py
+++ bert_exp/shaping/trainer.py
@@ -1,18 +1,57 @@
 # pyright: reportPrivateImportUsage=false
 from __future__ import annotations
 
-from omegaconf import DictConfig
+from typing import List
+from pl_bolts.callbacks import ORTCallback, PrintTableMetricsCallback
+import loguru
+from omegaconf import DictConfig, OmegaConf
 from pytorch_lightning import Trainer as PLTrainer
-from pytorch_lightning.callbacks import RichModelSummary, RichProgressBar
+from pytorch_lightning.callbacks import (
+    Callback,
+    DeviceStatsMonitor,
+    EarlyStopping,
+    GradientAccumulationScheduler,
+    ModelCheckpoint,
+    RichModelSummary,
+    RichProgressBar,
+)
 
 
 class Trainer(PLTrainer):
     def __init__(self, cfg: DictConfig) -> None:
-        trainer_cfg = cfg["trainer"]
-        callback_cfg = cfg["callbacks"]
+        self.cfg = cfg
+
+        trainer_cfg = self.cfg["trainer"]
+
+        loguru.logger.info(self.cfg)
+
+        super().__init__(callbacks=self._callbacks, **trainer_cfg)
+
+    @property
+    def _callbacks(self) -> List[Callback]:
+        callback_cfg = self.cfg["callbacks"]
 
         callbacks = []
-        if callback_cfg["rich"]:
-            callbacks += [RichModelSummary(), RichProgressBar()]
 
-        super().__init__(callbacks=callbacks, **trainer_cfg)
+        if callback_cfg["early_stopping"]:
+            callbacks.append(EarlyStopping())
+
+        if callback_cfg["device_stats"]:
+            callbacks.append(DeviceStatsMonitor())
+
+        if callback_cfg["grad_acc_scheduler"]:
+            callbacks.append(GradientAccumulationScheduler())
+
+        if ckpt := callback_cfg["checkpoint"]:
+            callbacks.append(ModelCheckpoint(ckpt))
+
+        if callback_cfg["onnx"]:
+            callbacks.append(ORTCallback())
+
+        if callback_cfg["rich"]:
+            callbacks.extend([RichModelSummary(), RichProgressBar()])
+
+        if callback_cfg["table_metrics"]:
+            callbacks.append(PrintTableMetricsCallback())
+
+        return callbacks
